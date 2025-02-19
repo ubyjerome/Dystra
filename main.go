27419fb584c9320fc9ea23a04f4109d148a665c7
ubyjerome <ubongabasijerome8@gmail.com>
2025-02-19 23:41:13 +0100

    Feat(Utilities): Initialized File Reader

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"fmt"
 	"ubyjerome/dystra/routines"
+	"ubyjerome/dystra/utilities"
 )
 
 func main() {
 	fmt.Println("Project Entry Point")
 	routine.ScanForIncludedFolders()
+
+	file, err := utilities.FetchFile("./.dystra.config.json")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(file)
 }
--- utilities/file-reader.go
+++ utilities/file-reader.go
@@ -1,1 +1,15 @@
 package utilities
+
+import (
+	"fmt"
+	"os"
+)
+
+
+func FetchFile(filePath string) (string, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", fmt.Errorf("failed to read file: %w", err)
+	}
+	return string(data), nil
+}
